f9438ef152b311dc4f84ac71d5e5cb9c566a475e
Joe Brown <brownj8@tcd.ie>
2022-04-03 02:01:37 +0100

    formatted print_receipt

--- python/shopping_cart.py
+++ python/shopping_cart.py
@@ -21,9 +21,36 @@ class ShoppingCart(IShoppingCart):
             self._contents[item_type] = self._contents[item_type] + number
 
     def print_receipt(self):
-        for key, value in self._contents.items():
-            price = self.pricer.get_price(key)
-            print(f"{key} - {value} - {price}")
+
+        total_cost = 0
+        for item_key, item_amount in self._contents.items():
+            price = self.pricer.get_price(item_key) * item_amount
+            price_string = str(price)
+            if price > 99:
+                euro = price_string[:-2]
+                cent = price_string[-2:]
+                price_string = "€{}.{}".format(euro, cent)
+            else:
+                euro = str(0)
+                if price < 10:
+                    cent = "0{}".format(price)
+                else:
+                    cent = price_string
+                price_string = "€{}.{}".format(euro, cent)
+            print("{item_key} - {item_amount} - {price}".format(item_key = item_key, item_amount = item_amount, price = price_string))
+            total_cost += price
+
+        total_cost_string = str(total_cost)
+        if total_cost > 99:
+            euro = total_cost_string[:-2]
+            cent = total_cost_string[-2:]
+            total_cost_string = "€{}.{}".format(euro, cent)
+        else:
+            euro = str(0)
+            cent = total_cost_string
+            total_cost_string = "€{}.{}".format(euro, cent)
+        print("Total cost: {}".format(total_cost_string))
+        
 
 class ShoppingCartCreator(ABC):
     """
--- python/tests.py
+++ python/tests.py
@@ -7,9 +7,12 @@ class ShoppingCartTest(unittest.TestCase):
     def test_print_receipt(self):
         sc = ShoppingCartConcreteCreator().operation()
         sc.add_item("apple", 2)
+        sc.add_item("banana", 4)
+        sc.print_receipt()
         with Capturing() as output:
             sc.print_receipt()
-        self.assertEqual("apple - 2 - 100", output[0])
+        self.assertEqual("apple - 2 - €2.00", output[0])
+        self.assertEqual("banana - 4 - €8.00", output[1])
 
     def test_doesnt_explode_on_mystery_item(self):
         sc = ShoppingCartConcreteCreator().operation()
@@ -18,8 +21,8 @@ class ShoppingCartTest(unittest.TestCase):
         sc.add_item("pear", 5)
         with Capturing() as output:
             sc.print_receipt()
-        self.assertEqual("apple - 2 - 100", output[0])
-        self.assertEqual("banana - 5 - 200", output[1])
-        self.assertEqual("pear - 5 - 0", output[2])
+        self.assertEqual("apple - 2 - €2.00", output[0])
+        self.assertEqual("banana - 5 - €10.00", output[1])
+        self.assertEqual("pear - 5 - €0.00", output[2])
 
 unittest.main(exit=False)
